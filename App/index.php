142bbbfb6a7a917346f10c79d9f6af3a32da667d
PabloValentin94 <pablo.valentin@fatec.sp.gov.br>
2025-09-16 12:13:40 -0300

    :sparkles: feat: Add a PHP base file.

---
+++ App/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    try {
+        $dsn = "mysql:host=Personal_MySQL_Server;dbname=Personal_App";
+
+        $connection = new PDO($dsn, "root", "root");
+
+        $test = $connection->query("Show database variables.");
+
+        while ($row = $test->fetch()) {
+            echo "<p>Name: " . $row[0] . " " . $row[1] . "</p>";
+        }
+    }
+    catch (Exception $ex) {
+        echo "Error: " . $ex->getMessage();
+    }
+
+?>
